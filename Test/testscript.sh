731349f532ead60e743c0ebbb320fcaeb1e3a3f7
AhmedAshrafHamouda <ahmedhamouda399@gmail.com>
2021-12-16 07:27:19 -0800

    try to fix host key file

--- Test/testscript.sh
+++ Test/testscript.sh
@@ -1,3 +1,2 @@
-python3 -m pytest ./Test/testospf.py --disable-warnings -s --verbose
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./demo.yml
+ansible-playbook ./demo.yml
 python3 -m pytest ./Test/testospf.py --disable-warnings -s --verbose
